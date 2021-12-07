a666948c452169a97ac47534b8c6560af45c79cb
kshyk <jj.turkiewicz@gmail.com>
2021-12-08 00:05:51 +0100

    added before each test hook

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-test('check navigation to introduction page', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await expect(page).toHaveTitle(/Playwright/);
-  const getStartedButton = page.locator('text=Get Started').first();
-  await expect(getStartedButton).toHaveAttribute('href', '/docs/intro');
-  await getStartedButton.click();
-  await expect(page.locator('text=Introduction').first()).toBeVisible();
+test.describe('home page', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://playwright.dev/');
+        await expect(page).toHaveTitle(/Playwright/);
+    });
+
+    test('introduction page should be displayed after click on get started button', async ({ page }) => {
+        const getStartedButton = page.locator('text=Get Started').first();
+        await expect(getStartedButton).toHaveAttribute('href', '/docs/intro');
+        await getStartedButton.click();
+        await expect(page.locator('text=Introduction').first()).toBeVisible();
+    });
 });
