2e431d55398f7b9d08327b8f15e8e28fdfdd9a64
kshyk <jj.turkiewicz@gmail.com>
2022-01-26 20:58:36 +0100

    fix: explicit-module-boundary-types

--- pageobjects/pajacyk/home-page.ts
+++ pageobjects/pajacyk/home-page.ts
@@ -11,15 +11,17 @@ export class HomePage {
     this.thanks = page.locator('.pajacyk__thankyou');
   }
 
-  async goto() {
+  async goto(): Promise<void> {
     this.page.setDefaultNavigationTimeout(0);
     await this.page.goto('https://www.pajacyk.pl/#index', {
       waitUntil: 'load',
       timeout: 0
     });
+    return;
   }
 
-  async clickBelly() {
+  async clickBelly(): Promise<void> {
     await this.bellyButton.first().click();
+    return;
   }
 }
